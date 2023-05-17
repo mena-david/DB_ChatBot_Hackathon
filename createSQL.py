b2b466774d831fc25d8463b7a9480ba7bcae6149
Joyce Fang <103448723+jo-fang@users.noreply.github.com>
2023-05-17 19:21:29 -0400

    Update createSQL.py

--- createSQL.py
+++ createSQL.py
@@ -1,7 +1,8 @@
+from db import *
 import openai
 import pinecone
 import constants
-from db import *
+import json
 from flask import Flask, request, jsonify
 import yaml
 
@@ -9,10 +10,11 @@ def read_yaml_config(config_file):
     with open(config_file, "r") as file:
         config = yaml.safe_load(file)
     return config
+
 config_file = "config.yml"
 config = read_yaml_config(config_file)
 
-app = Flask(__name__)
+db_config = config["database"]
 
 # Set the OpenAI API key and client
 openai.organization = config["api"]["openai-org"]
@@ -21,77 +23,108 @@ openai.Engine.list()
 
 pinecone.init(
     api_key=config["api"]["pinecone"],
-    environment="asia-northeast1-gcp"  # find next to API key in console
+    environment="us-central1-gcp"  # find next to API key in console
 )
 
+# Define your database schema as plain text here
+db_schema = constants.DB_SHCEMA
+
+conversation_examples = {
+    "examples": [
+        {"user": "Give me everything on user XXX", "sql": "SELECT name, id FROM users WHERE name=XXX"},
+        {"user": "Is Spencer Pearson a admin user?", "sql": "SELECT * FROM users WHERE name = 'Spencer Pearson' AND admin = 1"},
+        {"user": "How many runs has Firstname Lastname run", "sql": "SELECT COUNT(*) FROM runs WHERE user_id = (SELECT id FROM users WHERE name = 'Firstname Lastname');"},
+        {"user": "Get all me unique users that logged runs in the past 2 weeks?", "sql": "SELECT DISTINCT u.username FROM users u INNER JOIN runs r ON u.id = r.user_id WHERE r.created_at > DATE_SUB(NOW(), INTERVAL 2 WEEK);"},
+    ]
+}
+
+schema_and_examples = [db_schema] + [example["user"] for example in conversation_examples["examples"]]
+
 MODEL = "text-embedding-ada-002"
+res = openai.Embedding.create(
+    input=schema_and_examples, engine=MODEL
+)
 
-def setUpIndex():
-    # Context Set up
-    contex_items = constants.contex_items
+embedding = res.data[0].embedding
+example_embeddings = res.data[1:]
+id = 'db-chat-bot'
 
-    index = pinecone.Index('db-chatbot-personal')
+index = pinecone.Index('db-chatbot')
 
-    batch_num = 0
-    batches = []
-    for i in range(0, len(contex_items)):
-        batch_text = contex_items[i]
+example_vectors = [(f"example-{i}", e.embedding, {"text": schema_and_examples[i+1]}) for i, e in enumerate(res.data[1:])]
+meta = {'text': db_schema, 'examples': json.dumps(conversation_examples)}
 
-        res = openai.Embedding.create(
-            input=batch_text, engine="text-embedding-ada-002"
-        )
+index.upsert(vectors=[(id, embedding, meta)] + example_vectors)
 
-        embedding = res.data[0].embedding
-        id = 'batch' + '_' + str(batch_num)
-        meta = {'text': batch_text}
-
-        batches.append((id, embedding, meta))
-        batch_num += 1
-
-    index.upsert(vectors=batches)
-
-    return index
-
-#index = setUpIndex() #only needs to be done once
-index = pinecone.Index('db-chatbot-personal')
-
-def generate_gpt_response(prompt, context):
+def stream_gpt_response(prompt):
     try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": "system", "content": context},
-                      {"role": "user", "content": prompt}],
-            max_tokens=170,
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=160,
             n=1,
             stop=None,
             temperature=0.5,
+            stream=True
         )
-        return response.choices[0].message.content.strip()
+        for line in response: 
+            if 'text' in line['choices'][0]:
+                yield line['choices'][0]['text']
     except Exception as e:
         print(f"Error in generate_gpt_response: {e}")
         return "I'm sorry, but I couldn't complete your request. Please try again later."
 
-def process_user_input(user_input, connection):
+def generate_gpt_response(prompt):
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=160,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        print(f"Error in generate_gpt_response: {e}")
+        return "I'm sorry, but I couldn't complete your request. Please try again later."
+
+def generate_embedding(user_input):
     xq = openai.Embedding.create(input=user_input, engine=MODEL)['data'][0]['embedding']
     res = index.query([xq], top_k=9, include_metadata=True)
 
+def enrich_context(res):
     context = (
-        "You are a chatbot whose main purpose is to extract data from a Database using SQL queries. Here are some facts to keep in mind. "
-        "The Database ONLY has the following tables: users, organizations, organization_subscriptions, organization_members, plans, runs, entities, projects, alerts and alerts_subscriptions. "
+        "You are a chatbot whose main purpose is to extract data from a Database. Here are some facts to keep in mind"
+        "Database only has the following tables: users, organizations, organization_subscriptions, organization_members, runs, and plans"
+        "Enterprise users are those part of an organization who has a subscription with plan.name Enterprise"
+        "In order to determine what user belong to what organizations we need to look at the organization_members table"
+        "Users have an associated stripe_customer_id and if they are the billing user for an oganization that stripe_customer_id also belongs to that organization"
+        "Organizations can have multiple subscriptions and of those subscriptions of type Stripe, each has a stripe_subscription_id"
+        "stripe_subscription_ids are ONLY found on the organization_subscriptions table"
+        "admin users have a value of 1 on the admin column on the users table"
+        "Type of subscription is found on the organization_subscription table under the subscription_type column"
+        "Here is the necessary schema definitions for the different tables."
     )
 
-    schemas = "Here are the necessary database schema definitions for the different tables and all the available columns. Only refer to columns that exist on the schemas on the SQL query.\n"
-    schemas += constants.DB_SHCEMA
-
+    conversation_examples_text = ""
     for match in res['matches']:
         #print(f"{match['score']:.2f}: {match['metadata']['text']}")
-        context += str(match['metadata']['text'])
 
-    prompt = f"{schemas}\nThe request is as follows: {user_input}\n Only output a SQL Query:\n"
+        # Retrieve schema and conversation examples
+        if 'examples' in match['metadata']:
+            context += match['metadata']['text']
+            conversation_examples = json.loads(match['metadata']['examples'])
+            conversation_examples_text = "\n".join([f"Example {i + 1}\nUser: {ex['user']}\nSQL: {ex['sql']};" for i, ex in enumerate(conversation_examples["examples"])])
 
-    #print(prompt)
+    context += conversation_examples_text
+    return context
 
-    sql_query = generate_gpt_response(prompt, context)
+def generate_sql_prompt(user_input):
+    res = generate_embedding(user_input=user_input)
+    context = enrich_context(res=res)
+    prompt = f"{context}\nQuestion: {user_input}\nAnswer in the form of an SQL query:\n"
+    return prompt
 
     # You may need to validate the generated SQL query before executing it on the database
     print(sql_query + "\n")
@@ -114,7 +147,7 @@ def process_user_input(user_input, connection):
 
 def main_for_local_dev():
     # Connect to the database
-    connection = connect_to_db()
+    connection = connect_to_db(db_config)
     if not connection:
         print("Cannot connect to the database")
         return
@@ -133,24 +166,5 @@ def main_for_local_dev():
     # Close the database connection
     connection.close()
 
-# @app.route('/api/chat', methods=['POST'])
-# def chat():
-#     connection = connect_to_db()
-#     if not connection:
-#         print("Cannot connect to the database")
-#         return
-#
-#     user_input = request.json.get('user_input', '')
-#     if not user_input:
-#         return jsonify({'error': 'User input is required.'}), 400
-#
-#     response_text = process_user_input(user_input)
-#
-#     # Close the database connection
-#     connection.close()
-#
-#     return jsonify({'response': response_text[0] + "  " + response_text[1] })
-#
-# if __name__ == '__main__':
-#     #app.run(debug=True)
-#     main_for_local_dev()
+if __name__ == '__main__':
+    main()
